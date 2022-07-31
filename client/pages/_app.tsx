01eb77487a92eaa35ddcd0040637f8dcfc09f6fc
堀航大 <kodenginner4521@gmail.com>
2022-07-31 17:54:40 +0900

    認証機能作成

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,58 @@
 import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { User } from '../lib/models/user';
+
+export const AppContext = React.createContext(
+  {} as {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const path = router.pathname;
+  let isFirst = true;
+
+  const fetchData = async () => {
+    if (path === '/login') {
+      return;
+    }
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return router.push('/login');
+    }
+    try {
+      const res = await axios.get('http://localhost:3000/current_user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(
+        new User(res.data.id, res.data.name, res.data.email, res.data.password)
+      );
+    } catch {
+      if (isFirst) {
+        alert('tokenの認証が切れました。再度ログインしてください。');
+        localStorage.removeItem('token');
+        router.push('/login');
+      }
+    }
+    isFirst = false;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
 export default MyApp;
