15f4ae3aaab29694c4be45661736f08e935538f7
kod-source <kodenginner4521@gmail.com>
2022-08-02 18:39:20 +0900

    dbから

---
+++ api/app/interactor/user.go
@@ -0,0 +1,58 @@
+package interactor
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/kod-source/docker-goa-next/app/model"
+)
+
+type UserInteractor interface {
+	GetUser(ctx context.Context, id int) (*model.User, error)
+	GetUserByEmail(ctx context.Context, email, password string) (*model.User, error)
+}
+
+type userInteractor struct {
+	db *sql.DB
+}
+
+func NewUserInteractor(db *sql.DB) UserInteractor {
+	return userInteractor{
+		db: db,
+	}
+}
+
+func (u userInteractor) GetUser(ctx context.Context, id int) (*model.User, error) {
+	var user model.User
+	err := u.db.QueryRow("SELECT `id`, `name`, `email`, `password`, `created_at` FROM `user` WHERE `id` = ?", id).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u userInteractor) GetUserByEmail(ctx context.Context, email, password string) (*model.User, error) {
+	var user model.User
+	err := u.db.QueryRow(
+		"SELECT `id`, `name`, `email`, `password`, `created_at` FROM `user` WHERE `email` = ? AND `password` = ?",
+		email,
+		password,
+	).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
--- api/app/model/user.go
+++ api/app/model/user.go
@@ -1,10 +1,11 @@
 package model
 
 type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
 }
 
 var MockUser = User{
