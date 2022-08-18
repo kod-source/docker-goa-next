5caf0194b28b3cb17a41f020607dc7266adb4526
kod-source <kodenginner4521@gmail.com>
2022-08-18 17:29:05 +0900

    sql文の記述

--- api/app/interactor/comment.go
+++ api/app/interactor/comment.go
@@ -22,5 +22,40 @@ func NewCommentInteractor(db *sql.DB, tr repository.TimeRepository) CommentInter
 }
 
 func (c *commentInteractor) Create(ctx context.Context, postID int, text string, img *string) (*model.Comment, error) {
-	return nil, nil
+	var comment model.Comment
+	tx, err := c.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	ins, err := tx.Prepare(
+		"INSERT INTO comments(`post_id`, `text`, `img`, `created_at`, `updated_at`) VALUES(?,?,?,?,?)",
+	)
+	if err != nil {
+		return nil, err
+	}
+	res, err := ins.Exec(postID, text, img, c.tr.Now(), c.tr.Now())
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	err = tx.QueryRow(
+		"SELECT `id`, `post_id`, `text`, `img`, `created_at`, `updated_at` FROM comments WHERE id = ?", lastID,
+	).Scan(
+		&comment.ID,
+		&comment.PostID,
+		&comment.Text,
+		&comment.Img,
+		&comment.CreatedAt,
+		&comment.UpdatedAt,
+	)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	return &comment, tx.Commit()
 }
