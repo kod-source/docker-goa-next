aa893540c8de9cde9ff409e9dbfe2430a5b9b4ee
kod-source <kodenginner4521@gmail.com>
2022-11-23 23:26:21 +0900

    test

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -91,6 +91,7 @@ const Home: NextPage = () => {
   }, []);
 
   const onChangeInputFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("test")
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
       const reader = new FileReader();
