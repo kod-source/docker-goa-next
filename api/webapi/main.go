912da4536b326d48f9f46e2d677c1e03f7afd909
kod-source <kodenginner4521@gmail.com>
2022-11-30 18:22:50 +0900

    コメントの削除

--- api/webapi/main.go
+++ api/webapi/main.go
@@ -4,52 +4,15 @@ package main
 
 import (
 	"context"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// // Create service
-	// service := goa.New("docker_goa_next")
-
-	// // Mount middleware
-	// service.Use(middleware.RequestID())
-	// service.Use(middleware.LogRequest(true))
-	// service.Use(middleware.ErrorHandler(service, true))
-	// service.Use(middleware.Recover())
-
-	// app.UseJWTMiddleware(service, newAuthMiddleware())
-
-	// db, err := schema.NewDB()
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// defer db.Close()
-	// // Mount "operands" controller
-	// uu := usecase.NewUserUseCase(datastore.NewUserDatastore(db, repository.NewTimeRepositoy()), datastore.NewJWTDatastore(repository.NewTimeRepositoy()))
-	// pu := usecase.NewPostUseCase(datastore.NewPostDatastore(db, repository.NewTimeRepositoy()))
-	// c := NewOperandsController(service)
-	// app.MountOperandsController(service, c)
-	// a := NewAuthController(service, uu)
-	// app.MountAuthController(service, a)
-	// u := NewUsersController(service, uu)
-	// app.MountUsersController(service, u)
-	// p := NewPostsController(service, pu)
-	// app.MountPostsController(service, p)
-	// cc := NewCommentsController(service, usecase.NewCommentUsecase(datastore.NewCommentDatastore(db, repository.NewTimeRepositoy())))
-	// app.MountCommentsController(service, cc)
-	// l := NewLikesController(service, usecase.NewLikeUsecase(datastore.NewLikeDatastore(db)))
-	// app.MountLikesController(service, l)
-
-	// // Start service
-	// if err := service.ListenAndServe(":3000"); err != nil {
-	// 	service.LogError("startup", "err", err)
-	// }
-
 	app, err := NewApp(context.Background())
 	if err != nil {
 		panic(err)
 	}
+
+	// Start service
 	if err := app.srv.ListenAndServe(":3000"); err != nil {
 		app.srv.LogError("startup", "err", err)
 	}
