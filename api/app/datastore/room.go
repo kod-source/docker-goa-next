8c333f70c76f724787e9e6b569f9e4e7d5eb44e1
堀航大 <kodenginner4521@gmail.com>
2022-12-26 10:30:45 +0900

    datastoreの実装

--- api/app/datastore/room.go
+++ api/app/datastore/room.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/wire"
 	"github.com/kod-source/docker-goa-next/app/model"
 	"github.com/kod-source/docker-goa-next/app/repository"
+	"github.com/kod-source/docker-goa-next/app/schema"
 )
 
 var _ repository.RoomRepository = (*roomDatastore)(nil)
@@ -28,5 +29,113 @@ func NewRoomDatastore(db *sql.DB, tr repository.TimeRepository) *roomDatastore {
 
 // Create ルームの作成 DB処理
 func (rd *roomDatastore) Create(ctx context.Context, name string, isGroup bool, userIDs []model.UserID) (*model.RoomUser, error) {
-	return nil, nil
+	tx, err := rd.db.Begin()
+	if err != nil {
+		return nil, nil
+	}
+	defer tx.Rollback()
+
+	// Roomの作成
+	ins, err := tx.PrepareContext(
+		ctx,
+		"INSERT INTO `room`(`name`, `is_group`, `created_at`, `updated_at`) VALUES(?,?,?,?)",
+	)
+	if err != nil {
+		return nil, err
+	}
+	res, err := ins.ExecContext(ctx, name, isGroup, rd.tr.Now(), rd.tr.Now())
+	if err != nil {
+		return nil, err
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	// UserRoomの作成
+	stmt, err := tx.PrepareContext(
+		ctx,
+		"INSERT INTO `user_room`(`user_id`, `room_id`, `created_at`, `updated_at`) VALUES(?,?,?,?)",
+	)
+	if err != nil {
+		return nil, err
+	}
+	for _, userID := range userIDs {
+		go func() error {
+			if _, err := stmt.ExecContext(ctx, userID, lastID, rd.tr.Now(), rd.tr.Now()); err != nil {
+				return err
+			}
+			return nil
+		}()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var room schema.Room
+	var users []*schema.User
+	query := "SELECT `r`.`id`, `r`.`name`, `r`.`is_group`, `r`.`created_at`, `r`.`updated_at`, "
+	query += "`u`.`id`, `u`.`name`, `u`.`avatar`, `u`.`created_at` "
+	query += "FROM `room` AS `r` "
+	query += "INNER JOIN ( "
+	query += "SELECT `ur`.`room_id`, `ur`.`user_id`, `u`.`id`, `u`.`name`, `u.avatar`, `u`.`created_at` "
+	query += "FROM `user_room` AS `ur` "
+	query += "INNER JOIN `user` AS `u` "
+	query += "ON `ur`.`user_id` = `u`.`id` "
+	query += ") AS `u`"
+	query += "ON `r`.`id` = `u`.`room_id` "
+	query += "WHERE `r`.`id` = ? "
+	rows, err := tx.QueryContext(ctx, query, lastID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user schema.User
+
+		if err := rows.Scan(
+			&room.ID,
+			&room.Name,
+			&room.IsGroup,
+			&room.CreatedAt,
+			&room.UpdatedAt,
+			&user.ID,
+			&user.Name,
+			&user.Avatar,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	return rd.toModelRoomUser(room, users), tx.Commit()
+}
+
+func (rd *roomDatastore) toModelRoomUser(room schema.Room, users []*schema.User) *model.RoomUser {
+	var showUsers []*model.ShowUser
+	for _, u := range users {
+		showUser := &model.ShowUser{
+			ID:        model.UserID(u.ID),
+			Name:      u.Name,
+			CreatedAt: u.CreatedAt,
+		}
+		if u.Avatar.Valid {
+			showUser.Avatar = &u.Avatar.String
+		}
+
+		showUsers = append(showUsers, showUser)
+	}
+
+	return &model.RoomUser{
+		Room: model.Room{
+			ID:        model.RoomID(room.ID),
+			Name:      room.Name,
+			IsGroup:   room.IsGroup,
+			CreatedAt: room.CreatedAt,
+			UpdatedAt: room.UpdatedAt,
+		},
+		Users: showUsers,
+	}
 }
