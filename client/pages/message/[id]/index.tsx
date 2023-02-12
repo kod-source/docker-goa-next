981c69d7b0c30345a8b5aee46c770d28a67462cd
堀航大 <kodenginner4521@gmail.com>
2023-02-13 00:56:47 +0900

    メッセージのデータ取得

--- client/pages/message/[id]/index.tsx
+++ client/pages/message/[id]/index.tsx
@@ -1,8 +1,11 @@
 import { ShowRoom } from "lib/model/room";
+import { IndexThread } from "lib/model/thread";
 import { RoomRepository } from "lib/repository/room";
+import { ThreadRepository } from "lib/repository/thread";
 import { NextPage, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { AppContext } from "pages/_app";
+import { useCallback, useContext, useEffect, useState } from "react";
 
 interface Props {
     roomID: number;
@@ -10,20 +13,64 @@ interface Props {
 
 const ShowMessage: NextPage<Props> = ({ roomID }) => {
     const router = useRouter();
+    const { user } = useContext(AppContext);
     const [showRoom, setShowRoom] = useState<ShowRoom>();
+    const [indexThreads, setIndexThread] = useState<IndexThread[]>([]);
+    const [nextID, setNextID] = useState<number | null>(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [againFetch, setAgainFetch] = useState(false);
 
     const fetchData = async () => {
-        const showRoom = await RoomRepository.show(roomID);
-        setShowRoom(showRoom);
+        await RoomRepository.show(roomID).then((value) => setShowRoom(value));
     };
+
+    const fetchThreadData = async () => {
+        if (nextID == null) return;
+        setIsLoading(true);
+        const allThread = await ThreadRepository.getByRoom(roomID, nextID);
+        setNextID(allThread.nextID);
+        setIndexThread((old) => {
+            if (nextID === 0) {
+                return allThread.indexThreads;
+            }
+            return [...allThread.indexThreads, ...old];
+        });
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        if (againFetch) {
+            fetchThreadData();
+        }
+        window.addEventListener("scroll", changeBottom);
+        return () => window.removeEventListener("scroll", changeBottom);
+    }, [againFetch]);
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    const changeBottom = useCallback(() => {
+        const bottomPosition = document.body.offsetHeight - (window.scrollY + window.innerHeight);
+        if (bottomPosition < 0) {
+            setAgainFetch(true);
+            return;
+        }
+        setAgainFetch(false);
+    }, []);
+
     return (
         <>
-            <div>hello</div>
+            <div>
+              hello
+              {/* ルーム情報 */}
+            </div>
+            <div>
+              {/* メッセージの表示 */}
+            </div>
+            <div>
+              {/* メッセージの投稿 */}
+            </div>
         </>
     );
 };
