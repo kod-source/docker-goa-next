1097852af83db33f3925791d0360ed025792e18d
kod-source <kodenginner4521@gmail.com>
2023-03-01 22:31:01 +0900

    install api/oauth2/v2

--- api/app/service/google.go
+++ api/app/service/google.go
@@ -1,6 +1,14 @@
 package service
 
+import (
+	"context"
+
+	"github.com/kod-source/docker-goa-next/app/model"
+)
+
 type GoogleService interface {
 	// GetLoginURL ログインするためのリダレクトURLを取得
-	GetLoginURL(state string) (string)
+	GetLoginURL(state string) string
+	// GetUserInfo codeからUserの情報を返す
+	GetUserInfo(ctx context.Context, code string) (*model.User, error)
 }
--- api/webapi/webapi.go
+++ api/webapi/webapi.go
@@ -32,9 +32,9 @@ type appConfig struct {
 }
 
 type GoogleConfig struct {
-	ClientEndPoint string `env:"CLIENT_END_POINT"`
-	ClientID       string `env:"GOOGLE_CLIENT_ID"`
-	ClientSecret   string `env:"GOOGLE_CLIENT_SECRET"`
+	Endpoint     string `env:"END_POINT"`
+	ClientID     string `env:"GOOGLE_CLIENT_ID"`
+	ClientSecret string `env:"GOOGLE_CLIENT_SECRET"`
 }
 
 func (a *App) LaunchServer() error {
@@ -65,7 +65,7 @@ func newGoogleConfig() (*oauth2.Config, error) {
 		ClientSecret: gcfg.ClientSecret,
 		Endpoint:     googleOAuth.Endpoint,
 		Scopes:       []string{"openid"},
-		RedirectURL:  fmt.Sprintf("%s/auth/callback/google", gcfg.ClientEndPoint),
+		RedirectURL:  fmt.Sprintf("%s/api/v1/auth/callback/google", gcfg.Endpoint),
 	}, nil
 }
 
