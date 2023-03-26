f84871d6bc2e9c5924067dbeb118d83506d57c94
kod-source <kodenginner4521@gmail.com>
2023-03-26 21:45:37 +0900

    Repostiory層にトランザクションを渡すように修正

--- api/app/interactor/like.go
+++ api/app/interactor/like.go
@@ -2,6 +2,7 @@ package interactor
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/google/wire"
 	"github.com/kod-source/docker-goa-next/app/model"
@@ -19,18 +20,26 @@ var LikeInteractorSet = wire.NewSet(
 
 type likeInteractor struct {
 	lr repository.LikeRepository
+	db *sql.DB
 }
 
-func NewLikeInteractor(lr repository.LikeRepository) *likeInteractor {
-	return &likeInteractor{lr: lr}
+func NewLikeInteractor(lr repository.LikeRepository, db *sql.DB) *likeInteractor {
+	return &likeInteractor{lr: lr, db: db}
 }
 
 func (l *likeInteractor) Create(ctx context.Context, userID, postID int) (*model.Like, error) {
+	tx, err := l.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
 	if userID == 0 || postID == 0 {
 		return nil, myerrors.ErrBadRequestInt
 	}
-	like, err := l.lr.Create(ctx, userID, postID)
+	like, err := l.lr.Create(ctx, tx, userID, postID)
 	if err != nil {
+		return nil, err
+	}
+	if err := tx.Commit(); err != nil {
 		return nil, err
 	}
 
@@ -38,19 +47,33 @@ func (l *likeInteractor) Create(ctx context.Context, userID, postID int) (*model
 }
 
 func (l *likeInteractor) Delete(ctx context.Context, userID, postID int) error {
+	tx, err := l.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
 	if userID == 0 || postID == 0 {
 		return myerrors.ErrBadRequestInt
 	}
-	err := l.lr.Delete(ctx, userID, postID)
+	err = l.lr.Delete(ctx, tx, userID, postID)
 	if err != nil {
+		return err
+	}
+	if err := tx.Commit(); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (l *likeInteractor) GetPostIDs(ctx context.Context, userID int) ([]int, error) {
-	postIDs, err := l.lr.GetPostIDs(ctx, userID)
+	tx, err := l.db.BeginTx(ctx, nil)
 	if err != nil {
+		return nil, err
+	}
+	postIDs, err := l.lr.GetPostIDs(ctx, tx, userID)
+	if err != nil {
+		return nil, err
+	}
+	if err := tx.Commit(); err != nil {
 		return nil, err
 	}
 	return postIDs, nil
--- api/app/repository/like.go
+++ api/app/repository/like.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/kod-source/docker-goa-next/app/model"
 )
 
 type LikeRepository interface {
-	Create(ctx context.Context, userID, postID int) (*model.Like, error)
-	Delete(ctx context.Context, userID, postID int) error
-	GetPostIDs(ctx context.Context, userID int) ([]int, error)
+	Create(ctx context.Context, tx *sql.Tx, userID, postID int) (*model.Like, error)
+	Delete(ctx context.Context, tx *sql.Tx, userID, postID int) error
+	GetPostIDs(ctx context.Context, tx *sql.Tx, userID int) ([]int, error)
 }
