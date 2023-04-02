d357315358a42a09fd33c164ef3c28b4dada0e92
kod-source <kodenginner4521@gmail.com>
2023-04-02 18:35:29 +0900

    ルーム情報の表示

--- client/pages/message/[id]/index.tsx
+++ client/pages/message/[id]/index.tsx
@@ -10,6 +10,8 @@ import { AppContext } from "pages/_app";
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { MessageInput } from "lib/components/MessageInput";
+import { Avatar } from "@mui/material";
+import { isAxiosError } from "lib/axios";
 
 interface Props {
     roomID: number;
@@ -30,16 +32,26 @@ const ShowMessage: NextPage<Props> = ({ roomID }) => {
 
     const fetchThreadData = async () => {
         if (nextID == null) return;
-        setIsLoading(true);
-        const allThread = await ThreadRepository.getByRoom(roomID, nextID);
-        setNextID(allThread.nextID);
-        setIndexThread((old) => {
-            if (nextID === 0) {
-                return allThread.indexThreads;
+        try {
+            setIsLoading(true);
+            const allThread = await ThreadRepository.getByRoom(roomID, nextID);
+            setNextID(allThread.nextID);
+            setIndexThread((old) => {
+                if (nextID === 0) {
+                    return allThread.indexThreads;
+                }
+                return [...allThread.indexThreads, ...old];
+            });
+            setIsLoading(false);
+        } catch (e) {
+            if (isAxiosError(e)) {
+                const myAxiosError = e.response;
+                if (myAxiosError?.status === 404) {
+                    return;
+                }
+                return alert(myAxiosError?.statusText);
             }
-            return [...allThread.indexThreads, ...old];
-        });
-        setIsLoading(false);
+        }
     };
 
     useEffect(() => {
@@ -79,8 +91,44 @@ const ShowMessage: NextPage<Props> = ({ roomID }) => {
                 <h2>メッセージ一覧</h2>
             </div>
             <div>
-                hello
-                {/* ルーム情報 */}
+                {showRoom.room.isGroup ? (
+                    <div className='flex items-center'>
+                        <Avatar src={showRoom.room.img} />
+                        <p className='mx-5'>{showRoom.room.name}</p>
+                        <div className='flex items-center ml-auto border border-neutral-700 rounded-md p-2'>
+                            {showRoom.users
+                                .filter((u) => u.id !== user.id)
+                                .map((u) => (
+                                    <div key={u.id} className='w-6'>
+                                        <Avatar
+                                            src={u.avatar}
+                                            className={`object-cover rounded-full border-1 border-white transform`}
+                                        />
+                                    </div>
+                                ))}
+                            <p className='ml-5 opacity-70'>{showRoom.users.length}</p>
+                        </div>
+                    </div>
+                ) : (
+                    <div className='flex items-center'>
+                        {showRoom.users.map((u) => {
+                            if (u.id === user.id) {
+                                return;
+                            }
+                            return (
+                                <>
+                                    <div
+                                        onClick={() => router.push(`/users/${u.id}`)}
+                                        className='cursor-pointer hover:opacity-60'
+                                    >
+                                        <Avatar src={u.avatar} />
+                                    </div>
+                                    <p className='mx-5'>{u.name}</p>
+                                </>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
             <div>{/* メッセージの表示 */}</div>
             <div className='flex flex-col rounded-lg my-2 w-3/5 absolute bottom-10'>
