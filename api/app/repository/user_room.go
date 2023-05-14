8b9c76bf9f112042ca0d5d65e8191c3381b60735
kod-source <kodenginner4521@gmail.com>
2023-05-14 18:47:50 +0900

    gomockの導入

--- api/app/repository/user_room.go
+++ api/app/repository/user_room.go
@@ -6,6 +6,8 @@ import (
 	"github.com/kod-source/docker-goa-next/app/model"
 )
 
+//go:generate mockgen -source=./user_room.go -package=mock_repository -destination=./mock/user_room_repository.go
+
 type UserRoomRepository interface {
 	Create(ctx context.Context, roomID model.RoomID, userID model.UserID) (*model.UserRoom, error)
 	Delete(ctx context.Context, id model.UserRoomID) error
